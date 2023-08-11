0881b2d8757bbe0cd893057d8856c4f9bf8f0f8c
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-08-11 04:55:03 +0100

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,9 @@
-Commande ECHO activ‚e.
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for row in matrix:
+        squared_row = [num ** 2 for num in row]
+        squared_matrix.append(squared_row)
+    return squared_matrix
+
+
+
