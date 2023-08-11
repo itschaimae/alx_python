5057049624176b3ac9fd7117eab4068d62311e51
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-08-11 05:58:54 +0100

    Update 5-raise_exception_msg.py

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,11 +1,9 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
 
-raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
-
 try:
     raise_exception_msg("This is a custom exception message")
-except NameError as ne:
-    print("Caught exception:", ne)
+except NameError as e:
+    print("Caught exception:", e)
 
 print("C is fun")
