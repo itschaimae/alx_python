c7fb223e19fe4f59a1a0e55d96e7b45f64b35a1c
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-08-11 03:29:57 +0100

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,1 +1,7 @@
-Commande ECHO activ‚e.
+for tens_digit in range(10):
+    for ones_digit in range(tens_digit + 1, 10):
+        if tens_digit < 9 or ones_digit < 9:
+            print("{:d}{:d}, ".format(tens_digit, ones_digit), end="")
+        else:
+            print("{:d}{:d}".format(tens_digit, ones_digit))
+
