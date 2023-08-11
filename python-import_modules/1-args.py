306746ff63710b4f0b2b5fa28010557696bc8158
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-08-11 04:31:37 +0100

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,1 +1,10 @@
-Commande ECHO activ‚e.
+import sys
+
+args = sys.argv[1:] 
+
+num_args = len(args)
+arg_plural = "argument" if num_args == 1 else "arguments"
+
+print("Number of {}{}:{}".format(" " if num_args > 0 else "", arg_plural, "." if num_args == 0 else ""))
+for i, arg in enumerate(args, start=1):
+    print("{}: {}".format(i, arg))
