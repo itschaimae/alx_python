4c940ad9e89a8d40c841e4b72afd5d4c49d70b37
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-09-05 17:49:21 +0100

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,15 @@
 import sys
 
 def main():
-    args = sys.argv[1:] 
-
+    args = sys.argv[1:]  # Exclude the script name from the arguments
     num_args = len(args)
-    arg_plural = "argument" if num_args == 1 else "arguments"
 
-    print("{} {}:".format(num_args, arg_plural))
-    for i, arg in enumerate(args, start=1):
-        print("{}: {}".format(i, arg))
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
+        for i, arg in enumerate(args, 1):
+            print(f"{i}: {arg}")
 
 if __name__ == "__main__":
     main()
