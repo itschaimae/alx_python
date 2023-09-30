4eb984dfd8eadaa0a4e02bcd11bcf1c8ebd005b9
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-09-30 04:03:38 +0100

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,5 @@
 class Square:
-    """
-    This class represents a square.
-
-    Attributes:
-        __size (int): The size of the square's sides.
-    """
-
+    
     def __init__(self, size):
-        """
-        Initializes a new Square instance.
-
-        Args:
-            size (int): The size of the square's sides.
-        """
+        
         self.__size = size
