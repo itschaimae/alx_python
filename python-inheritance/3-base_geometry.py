e699459a8e0db582efc04187ca6eebad2961e182
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-10-02 20:48:49 +0100

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,1 +1,11 @@
-Commande ECHO activ‚e.
+"""
+Module: 3-base_geometry
+This module defines an empty class, BaseGeometry.
+"""
+
+class BaseGeometry:
+    """
+    An empty class representing basic geometry.
+    """
+    pass
+
