91cd30d3b33512197af1a8e7e64b03e627089716
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-10-02 20:26:49 +0100

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -23,6 +23,12 @@ Classes:
 """
 
 class Square:
+    """Classes:
+    Square:
+        This class provides a basic representation of a square with a size attribute.
+
+        Attributes:
+            size (int): The size of the square's sides."""
     def __init__(self, size=0):
         """
         Initializes a new Square instance with a given size.
