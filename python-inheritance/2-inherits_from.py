3dac7b0977bed0da6635de7cc94424ccbcad672c
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-10-02 20:48:04 +0100

    Update 2-inherits_from.py

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,1 +1,17 @@
-Commande ECHO activ‚e.
+"""
+Module: 2-inherits_from
+This module contains a function to check if an object is an instance of a class that directly or indirectly inherits from a specified class.
+"""
+
+def inherits_from(obj, a_class):
+    """
+    Check if an object is an instance of a class that inherits (directly or indirectly) from the specified class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of a subclass of the specified class; otherwise, False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
