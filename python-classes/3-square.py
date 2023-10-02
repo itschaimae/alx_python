dbb3c66795585473a11eab4e17f917c2c56d415a
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-10-02 20:26:17 +0100

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -23,12 +23,6 @@ Classes:
 """
 
 class Square:
-    """Classes:
-    Square:
-        This class provides a basic representation of a square with a size attribute.
-
-        Attributes:
-            size (int): The size of the square's sides."""
     def __init__(self, size=0):
         """
         Initializes a new Square instance with a given size.
@@ -45,6 +39,34 @@ class Square:
     @property
     def size(self):
         """
-        int: The size of the square
+        int: The size of the square's sides.
+        """
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square's sides.
 
+        Args:
+            value (int): The new size for the square's sides.
 
+        Raises:
+            TypeError: If the provided value is not an integer.
+            ValueError: If the provided value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
