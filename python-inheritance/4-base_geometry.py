525914041e558c8fb9ed892654b7501b86312621
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-10-02 20:49:17 +0100

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,1 +1,18 @@
-Commande ECHO activ‚e.
+"""
+Module: 4-base_geometry
+This module defines a class, BaseGeometry, with an area() method.
+"""
+
+class BaseGeometry:
+    """
+    A class representing basic geometry with an area() method.
+    """
+    def area(self):
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+            Exception: This method is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
