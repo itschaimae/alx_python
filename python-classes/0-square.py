8c1e90be118b5ce8c9824adb3496564c319be229
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-10-02 20:15:16 +0100

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,15 +1,20 @@
 """
 Module: square
 
-This module defines a simple class, Square, that represents a square with private attributes for size.
+This module defines a class, Square, that represents a square with a private size attribute.
 
 Classes:
     Square:
-        This class represents a square with methods to manipulate its size and calculate area and perimeter.
+        This class provides a basic representation of a square with an attribute for its side length.
 """
+
 class Square:
-    
-    
     def __init__(self, size):
-        
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size: The size of the square's sides.
+        """
         self.__size = size
+
