a6de43cc97b87dc2320faa65b2d152dfdc9500de
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-10-03 00:39:58 +0100

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,35 @@
 
-class Base:
-    __nb_objects = 0
+"""
+models.base
 
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+This module defines the Base class, which serves as the foundation for managing ID attributes in other classes.
+
+Classes:
+    - Base: The Base class for managing IDs.
+
+Module Attributes:
+    - __all__ (List[str]): A list of module names that should be imported when using "from models.base import *".
+
+"""
+
+class Base:
+    """
+    The Base class serves as the foundation for managing ID attributes in other classes.
+
+    Attributes:
+        __nb_objects (int): A private class attribute used to generate unique IDs.
+
+    Args:
+        id (int, optional): An integer representing the ID. If provided, the instance is assigned the given ID.
+            If not provided, a new ID is generated automatically.
+
+    Attributes:
+        id (int): A public instance attribute representing the ID of the instance.
+
+    Methods:
+        __init__(self, id=None): Constructor method for creating a new Base instance.
+    """
+
+    __nb_objects
+
 
