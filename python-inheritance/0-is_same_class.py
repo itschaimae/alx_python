92949b78ab06f9bc4b6e3f8cf69bb5295a8d4c62
Chaimae <136246626+itschaimae@users.noreply.github.com>
2023-10-02 20:44:30 +0100

    Update 0-is_same_class.py

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,1 +1,11 @@
-Commande ECHO activ‚e.
+def is_same_class(obj, a_class):
+    return type(obj) == a_class
+
+# Test cases
+if is_same_class(1, int):
+    print("1 is an instance of the class int")
+if is_same_class(1, float):
+    print("1 is an instance of the class float")
+if is_same_class(1, object):
+    print("1 is an instance of the class object")
+
